eae7997cedc18d1fb811d20cfacd8a41a6096ff7
Joseph Goulden <joegoulden@protonmail.com>
2020-01-01 21:26:39 +0000

    chore: add build.rs to install hooks

---
+++ build.rs
@@ -0,0 +1,21 @@
+extern crate built;
+
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+	// Setting up git hooks
+	let git_hooks = format!("git config core.hooksPath {}", PathBuf::from("./.hooks").to_str().unwrap());
+
+	if cfg!(target_os = "windows") {
+		Command::new("cmd")
+			.args(&["/C", &git_hooks])
+			.output()
+			.expect("failed to execute git config for hooks");
+	} else {
+		Command::new("sh")
+			.args(&["-c", &git_hooks])
+			.output()
+			.expect("failed to execute git config for hooks");
+	}
+}
