0e26cb24a33e728d419b963b3876264cbf4904c8
Joseph Goulden <joegoulden@protonmail.com>
2020-09-25 18:21:27 +0100

    perf: use parking lot in hammersbald for small performance increase ~2%

--- hammersbald/src/asyncfile.rs
+++ hammersbald/src/asyncfile.rs
@@ -21,8 +21,9 @@ use crate::page::Page;
 use crate::pagedfile::PagedFile;
 use crate::pref::PRef;
 use crate::Error;
+use parking_lot::{Condvar, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::Arc;
 use std::thread;
 
 pub struct AsyncFile {
@@ -61,12 +62,12 @@ impl AsyncFile {
 	}
 
 	fn background(inner: Arc<AsyncFileInner>) {
-		let mut queue = inner.queue.lock().expect("page queue lock poisoned");
+		let mut queue = inner.queue.lock();
 		while inner.run.load(Ordering::Acquire) {
 			while queue.is_empty() {
-				queue = inner.work.wait(queue).expect("page queue lock poisoned");
+				inner.work.wait(&mut queue);
 			}
-			let mut file = inner.file.lock().expect("file lock poisoned");
+			let mut file = inner.file.lock();
 
 			for page in queue.iter() {
 				file.update_page(page.clone()).expect("error in async file writer");
@@ -77,8 +78,9 @@ impl AsyncFile {
 	}
 
 	fn read_in_queue(&self, pref: PRef) -> Result<Option<Page>, Error> {
-		let queue = self.inner.queue.lock().expect("page queue lock poisoned");
+		let queue = self.inner.queue.lock();
 		let mut result = None;
+		// Get the latest update to the page.
 		for page in queue.iter() {
 			if page.pref() == pref.this_page() {
 				result = Some(page);
@@ -93,20 +95,20 @@ impl PagedFile for AsyncFile {
 		if let Some(page) = self.read_in_queue(pref)? {
 			return Ok(Some(page));
 		}
-		let file = self.inner.file.lock().expect("file lock poisoned");
+		let file = self.inner.file.lock();
 		file.read_page(pref)
 	}
 
 	fn len(&self) -> Result<u64, Error> {
-		self.inner.file.lock().unwrap().len()
+		self.inner.file.lock().len()
 	}
 
 	fn truncate(&mut self, new_len: u64) -> Result<(), Error> {
-		self.inner.file.lock().unwrap().truncate(new_len)
+		self.inner.file.lock().truncate(new_len)
 	}
 
 	fn sync(&self) -> Result<(), Error> {
-		self.inner.file.lock().unwrap().sync()
+		self.inner.file.lock().sync()
 	}
 
 	fn shutdown(&mut self) -> Result<(), Error> {
@@ -116,18 +118,18 @@ impl PagedFile for AsyncFile {
 	}
 
 	fn update_page(&mut self, page: Page) -> Result<u64, Error> {
-		let mut queue = self.inner.queue.lock().unwrap();
+		let mut queue = self.inner.queue.lock();
 		queue.push(page);
 		self.inner.work.notify_one();
 		Ok(0)
 	}
 
 	fn flush(&mut self) -> Result<(), Error> {
-		let mut queue = self.inner.queue.lock().unwrap();
+		let mut queue = self.inner.queue.lock();
 		self.inner.work.notify_one();
 		while !queue.is_empty() {
-			queue = self.inner.flushed.wait(queue).unwrap();
+			self.inner.flushed.wait(&mut queue);
 		}
-		self.inner.file.lock().unwrap().flush()
+		self.inner.file.lock().flush()
 	}
 }
