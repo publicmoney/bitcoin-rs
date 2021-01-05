0edbce32345c18081efcb57038f850d9e575133d
Joseph Goulden <joegoulden@protonmail.com>
2021-01-05 12:33:24 +0000

    chore: fix build

--- bitcoin-rs/src/commands/stats.rs
+++ bitcoin-rs/src/commands/stats.rs
@@ -5,7 +5,7 @@ pub fn stats(cfg: &Config) -> Result<(), String> {
 	let db = db::RawDatabase::persistent(&app_path(&cfg.data_dir, "db"), cfg.db_cache).unwrap();
 	info!("Getting database statistics. This may take a while.");
 	db.stats().unwrap();
-	db.as_store().shutdown();
+	db.shutdown().unwrap();
 	info!("Finished");
 	Ok(())
 }
