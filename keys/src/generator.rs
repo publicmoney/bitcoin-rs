45238ed55158c5138de60c657f30f57e4007c1ad
Joseph Goulden <joegoulden@protonmail.com>
2021-01-09 11:46:38 +0000

    chore: update secp256k1 lib and uint

--- keys/src/generator.rs
+++ keys/src/generator.rs
@@ -1,6 +1,7 @@
 use crate::network::Network;
 use crate::{Error, KeyPair, SECP256K1};
-use rand::rngs::OsRng;
+use secp256k1::rand::rngs::StdRng;
+use secp256k1::rand::FromEntropy;
 
 pub trait Generator {
 	fn generate(&self) -> Result<KeyPair, Error>;
@@ -19,7 +20,8 @@ impl Random {
 impl Generator for Random {
 	fn generate(&self) -> Result<KeyPair, Error> {
 		let context = &SECP256K1;
-		let mut rng = OsRng::new().map_err(|_| Error::FailedKeyGeneration)?;
+
+		let mut rng = StdRng::from_entropy();
 		let (secret, public) = context.generate_keypair(&mut rng);
 		Ok(KeyPair::from_keypair(secret, public, self.network))
 	}
